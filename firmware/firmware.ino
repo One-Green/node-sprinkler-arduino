407fe82e248451e2fd69a4ea448295067d4b011b
Vigneswaran <shanmugathas.vigneswaran@outlook.fr>
2020-11-02 18:40:22 +0100

    feat(firmware.ino):
    - dev include CHECK_NODE_TAG_DUPLICATE to bypass registry tag duplicate
    - dev include OGIO Input/Output class handler

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -16,45 +16,57 @@
 
 #include "OGDisplay.h"
 #include "OGApiHandler.h"
+#include "OGIO.h"
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
+#include <EspMQTTClient.h>
 
+char *NODE_TYPE = "sprinkler";
+char *NODE_TAG = "orchid";
+bool CHECK_NODE_TAG_DUPLICATE = false;
 
-char* NODE_TYPE = "sprinkler" ;
-char* NODE_TAG  = "orchid" ;
+char *WIFI_SSID = "*";
+char *WIFI_PASSWORD = "*";
 
-char* WIFI_SSID = "*";
-char* WIFI_PASSWORD  = "*";
+char *API_GATEWAY_URL = "http://api.af120153-db6a-4fdd-a81b-6d902b00e936.nodes.k8s.fr-par.scw.cloud";
+char *API_GATEWAY_BASIC_AUTH_USER = "";
+char *API_GATEWAY_BASIC_AUTH_PASSWORD = "";
 
-char* API_GATEWAY_URL = "http://api.af120153-db6a-4fdd-a81b-6d902b00e936.nodes.k8s.fr-par.scw.cloud";
-char* API_GATEWAY_BASIC_AUTH_USER = "";
-char* API_GATEWAY_BASIC_AUTH_PASSWORD = "";
-
-char* MQTT_SERVER = "af120153-db6a-4fdd-a81b-6d902b00e936.nodes.k8s.fr-par.scw.cloud";
+char *MQTT_SERVER = "af120153-db6a-4fdd-a81b-6d902b00e936.nodes.k8s.fr-par.scw.cloud";
 int MQTT_PORT = 32500;
+
+// ADC to MAX = 100% and ADC tp MIN = 0% calibration
+// Used for mapping
+int SOIL_MOISTURE_ADC_MAX = 2330;
+int SOIL_MOISTURE_ADC_MIN = 1390;
 
 bool registered = false;
 
 
 DisplayLib displayLib;
 OGApiHandler apiHandler;
+OGIO io_handler;
+
+
 
 void setup(void) {
 
 	Serial.begin(115200);
 	displayLib.initR();
+	// set pin mode etc ...
+	io_handler.initR();
 
 	delay(1000);
 	Serial.println("\n");
 
 	displayLib.initWifi();
-	Serial.print("Connecting to wifi "); Serial.print(WIFI_SSID);
+	Serial.print("Connecting to wifi ");
+	Serial.print(WIFI_SSID);
 	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
 
-	while(WiFi.status() != WL_CONNECTED)
-	{
+	while (WiFi.status() != WL_CONNECTED) {
 		Serial.print(".");
 		delay(100);
 	}
@@ -68,25 +80,36 @@ void setup(void) {
 	displayLib.printHeader(WIFI_SSID, WiFi.localIP(), NODE_TYPE, NODE_TAG);
 
 
-	if(WiFi.status() == WL_CONNECTED){
+	if (WiFi.status() == WL_CONNECTED) {
 
-		registered = apiHandler.registerNodeTag(
-				NODE_TAG,
-				API_GATEWAY_URL,
-				API_GATEWAY_BASIC_AUTH_USER,
-				API_GATEWAY_BASIC_AUTH_PASSWORD
-				);
+		if (CHECK_NODE_TAG_DUPLICATE) {
+			registered = apiHandler.registerNodeTag(
+					NODE_TAG,
+					API_GATEWAY_URL,
+					API_GATEWAY_BASIC_AUTH_USER,
+					API_GATEWAY_BASIC_AUTH_PASSWORD
+			);
+
+		} else {
+			registered = true;
+		}
+
 	}
-
 }
 
 void loop() {
-	if (registered){
-		Serial.println("Registered");
-	}
-	else {
+
+	if (registered) {
+		int rawSoilMoisture = io_handler.getMoistureLevelADC();
+		int soilMoisture = io_handler.getMoistureLevel(
+				SOIL_MOISTURE_ADC_MIN, SOIL_MOISTURE_ADC_MAX,
+				100, 0);
+		Serial.println("Soil moisture ADC=" + String(rawSoilMoisture) + "/" + "LEVEL=" + String(soilMoisture));
+
+	} else {
 		Serial.println("Not registered, tag is already in database, remove it first");
 		displayLib.printRegistryError();
 	}
 
+	delay(500);
 }
