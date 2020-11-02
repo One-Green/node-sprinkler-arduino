a4b1ee12a2084f8f1b67920622454c0254850f74
Vigneswaran <shanmugathas.vigneswaran@outlook.fr>
2020-11-02 14:46:50 +0100

    feat(firmware.ino): include Node Tag registration to Api Gateway, ensure used Sprinkler is unique

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,22 +1,44 @@
+/*
+ * Sprinkler ESP32 Arduino Firmware
+ * https://github.com/One-Green/node-sprinkler-arduino
+ *
+ *
+ * Work with this framework
+ * One-Green : open source framework for plant cultivation including web server and microcontroller framework
+ * https://github.com/One-Green/plant-keeper-master
+ *
+ * License : Creative Commons Legal Code - CC0 1.0 Universal
+ * Author: Shanmugathas Vigneswaran
+ * mail: shanmugathas.vigneswaran@outlook.fr
+ *
+ *
+ * */
+
 #include "OGDisplay.h"
+#include "OGApiHandler.h"
 #include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 
 
 char* NODE_TYPE = "sprinkler" ;
 char* NODE_TAG  = "orchid" ;
+
 char* WIFI_SSID = "*";
 char* WIFI_PASSWORD  = "*";
-char* API_GATEWAY_URL = "http://api.af120153-db6a-4fdd-a81b-6d902b00e936.nodes.k8s.fr-par.scw.cloud/sprinkler/registry";
+
+char* API_GATEWAY_URL = "http://api.af120153-db6a-4fdd-a81b-6d902b00e936.nodes.k8s.fr-par.scw.cloud";
 char* API_GATEWAY_BASIC_AUTH_USER = "";
 char* API_GATEWAY_BASIC_AUTH_PASSWORD = "";
-int API_GATEWAY_PORT = 0;
+
 char* MQTT_SERVER = "af120153-db6a-4fdd-a81b-6d902b00e936.nodes.k8s.fr-par.scw.cloud";
 int MQTT_PORT = 32500;
+
 bool registered = false;
 
 
 DisplayLib displayLib;
-
+OGApiHandler apiHandler;
 
 void setup(void) {
 
@@ -45,9 +67,26 @@ void setup(void) {
 
 	displayLib.printHeader(WIFI_SSID, WiFi.localIP(), NODE_TYPE, NODE_TAG);
 
+
+	if(WiFi.status() == WL_CONNECTED){
+
+		registered = apiHandler.registerNodeTag(
+				NODE_TAG,
+				API_GATEWAY_URL,
+				API_GATEWAY_BASIC_AUTH_USER,
+				API_GATEWAY_BASIC_AUTH_PASSWORD
+				);
+	}
+
 }
 
 void loop() {
+	if (registered){
+		Serial.println("Registered");
+	}
+	else {
+		Serial.println("Not registered, tag is already in database, remove it first");
+		displayLib.printRegistryError();
+	}
 
-	delay(100);
 }
